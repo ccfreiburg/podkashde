e6e290229f91cbb2a50fe68fdc67a914b563c757
Alex Roehm <alex@roehms.de>
2022-04-26 17:06:35 +0200

    Remove Errors and Add TS support

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,30 @@
 module.exports = {
-    root: true,
-    env: {
-      node: true,
-      browser: true
-    },
-    extends: [
-      "plugin:vue/recommended",
-      "eslint:recommended",
-      "prettier/vue",
-      "plugin:prettier/recommended"
-    ],
-    rules: {
-      "vue/component-name-in-template-casing": ["error", "PascalCase"],
-      "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-      "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off"
-    },
-    globals: {
-      $nuxt: true
-    },
-    parserOptions: {
-      parser: "babel-eslint"
-    }
-  };
+  root: true,
+  env: {
+    node: true,
+    browser: true,
+  },
+  extends: [
+    "plugin:vue/recommended",
+    "eslint:recommended",
+    "prettier/vue",
+    "plugin:prettier/recommended",
+    "@nuxtjs/eslint-config-typescript",
+  ],
+  rules: {
+    "vue/component-name-in-template-casing": ["error", "PascalCase"],
+    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
+  },
+  globals: {
+    $nuxt: true,
+  },
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ["./tsconfig.json"],
+  },
+  //parserOptions: {
+  //  parser: "babel-eslint",
+  //},
+};
