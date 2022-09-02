598ea667f1714a03232dad9cd91f6fc72ccbf8cc
Alex Roehm <alex@roehms.de>
2022-09-02 14:22:53 +0000

    ui testing first test

--- tests/e2e/app.test.ts
+++ tests/e2e/app.test.ts
@@ -1,14 +1,13 @@
-import { describe, it } from 'vitest'
-import { setup, $fetch } from '@nuxt/test-utils-edge'
+import { describe, expect, it } from 'vitest'
+import { setup, $fetch, url } from '@nuxt/test-utils-edge'
 
 describe('App', async () => {
   await setup({
     server: true
   })
-  it('my test', () => {
-    // ...
-    const html = $fetch('/').then(()=>{ 
-    console.log(html)
-  })
+  it('my test', async () => {
+    const thePage = url('/page')
+    const html = await $fetch('/')
+    expect(html).toContain("Login")
 })
 })
